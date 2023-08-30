c75e8de5409155eb0155e2ed7a4e24e0fdfab68a
DrGass <80747033+DrGass@users.noreply.github.com>
2023-08-30 14:59:49 +0200

    Readme fix

--- app/modules/database.py
+++ app/modules/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password123@localhost:5432/FizjoMarcin"
+hostname = "localhost"
+# hostname = "127.0.0.2"
+SQLALCHEMY_DATABASE_URL = f"postgresql://user:password123@{hostname}:5432/FizjoMarcin"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
