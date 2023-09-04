97fea4e3e4833854da172ae856c45ba21b660a22
DrGass <80747033+DrGass@users.noreply.github.com>
2023-09-04 13:57:58 +0200

    readme update

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Simple FastAPI website, training for work and in future website for my brother.
 In order to make the docker work i needed to add this PYTHON PATH variable to Dockerfile because otherwise there was no option for it to work.
 
 And in order to make uvicorn start on docker i needed to manually insert ip adress from etc/hosts because it couldn't translate hostname into ip. so in file database.py the "hostname" variable needs to be changed to "127.0.0.2"
+
+## FizjoMarcin\app\routers\authentication.py file is a big mess but i figured way to repair authentification. I'll refactor it tommorow. Now i go talk to rubber duck and eat some calories, then I'll conquer alembic.
--- app/routers/patient.py
+++ app/routers/patient.py
@@ -6,7 +6,7 @@ from app.modules.database import get_db
 import app.modules.schemas.patient_schema as patient_schema
 import app.modules.models.patient_model as patient_model
 import app.modules.schemas.user_schema as user_schema
-import app.routers.authentication as auth
+import app.modules.auth.oauth2 as oauth2
 
 router = APIRouter(prefix="/patient", tags=["Patient"])
 
@@ -14,7 +14,7 @@ router = APIRouter(prefix="/patient", tags=["Patient"])
 @router.get("/", response_model=list[patient_schema.ShowPatient])
 def get_all(
     db: Session = Depends(get_db),
-    get_current_user: user_schema.User = Depends(auth.get_current_user),
+    get_current_user: user_schema.User = Depends(oauth2.get_current_user),
 ):
     patients = db.query(patient_model.Patient).all()
     return patients
@@ -28,7 +28,7 @@ def get_all(
 def get_by_id(
     id: int,
     db: Session = Depends(get_db),
-    get_current_user: user_schema.User = Depends(auth.get_current_user),
+    get_current_user: user_schema.User = Depends(oauth2.get_current_user),
 ):
     patient = patient_model.get_patient_by_id(id, db)
     if not patient:
@@ -43,7 +43,7 @@ def get_by_id(
 def create(
     request: patient_schema.NewPatient,
     db: Session = Depends(get_db),
-    get_current_user: user_schema.User = Depends(auth.get_current_user),
+    get_current_user: user_schema.User = Depends(oauth2.get_current_user),
 ):
     new_patient = patient_model.Patient(**request.model_dump())
     new_patient.owner_id = (request.id,)
@@ -57,7 +57,7 @@ def create(
 def destroy(
     id: int,
     db: Session = Depends(get_db),
-    get_current_user: user_schema.User = Depends(auth.get_current_user),
+    get_current_user: user_schema.User = Depends(oauth2.get_current_user),
 ):
     patient_model.delete_patient(id, db)
     db.commit()
@@ -69,7 +69,7 @@ def update(
     id,
     request: patient_schema.NewPatient,
     db: Session = Depends(get_db),
-    get_current_user: user_schema.User = Depends(auth.get_current_user),
+    get_current_user: user_schema.User = Depends(oauth2.get_current_user),
 ):
     patient_model.update_patient(id, db, request)
     db.commit()
