968d62cff95121c784329db42f2c326ab036194e
DrGass <80747033+DrGass@users.noreply.github.com>
2023-09-04 14:50:12 +0200

    a bit of a cleanup

--- app/routers/authentication.py
+++ app/routers/authentication.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
@@ -7,12 +8,11 @@ from sqlalchemy.orm import Session
 
 router = APIRouter(tags=["Authentication"])
 
-from app.modules.database import get_db
-
 # from app.modules.auth.token import create_access_token
 import app.modules.models.user_model as user_model
 import app.modules.schemas.token_schema as token_schema
 import app.modules.schemas.user_schema as user_schema
+from app.modules.database import get_db
 
 # from app.modules.auth.oauth2 import get_current_user
 
@@ -58,12 +58,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    print("to encode", to_encode)
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    print("to encode after update", to_encode)
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    print("already encoded: ", encoded_jwt)
     return encoded_jwt
 
 
@@ -89,14 +86,11 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        print("przed token", token)
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        print("po token ", token)
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
         token_data = token_schema.TokenData(username=username)
-        print("token_data", token_data)
         return token_data
 
     except JWTError:
@@ -113,24 +107,9 @@ async def login_for_access_token(
         .filter(user_model.User.username == form_data.username)
         .first()
     )
-    print(user)
     access_token = create_access_token(data={"sub": user.username})
-    print("access_token w poscie:", access_token)
     return {"access_token": access_token, "token_type": "bearer"}
-
-
-@router.get("/users/me/", response_model=user_schema.User)
-async def read_users_me(current_user: dict = Depends(get_current_user)):
-    return current_user
-
 
 @router.get("/items/")
 async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"token": token}
-
-
-# @app.get("/users/me/items/")
-# async def read_own_items(
-#     current_user: Annotated[User, Depends(get_current_active_user)]
-# ):
-#     return [{"item_id": "Foo", "owner": current_user.username}]
